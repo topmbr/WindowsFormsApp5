e13da134e096c2e26ed530c3b2a5428019f943ef
topmbr <centerm9911@gmail.com>
2023-09-01 21:34:59 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.textBoxTask = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(328, 168);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 0;
+            this.buttonEdit.Text = "button1";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(445, 168);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "button2";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(231, 168);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "button3";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.ItemHeight = 16;
+            this.listBoxTasks.Location = new System.Drawing.Point(310, 230);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(120, 84);
+            this.listBoxTasks.TabIndex = 3;
+            // 
+            // textBoxTask
+            // 
+            this.textBoxTask.Location = new System.Drawing.Point(310, 93);
+            this.textBoxTask.Name = "textBoxTask";
+            this.textBoxTask.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTask.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxTask);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ListBox listBoxTasks;
+        private System.Windows.Forms.TextBox textBoxTask;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // private ListBox listBoxTasks = new ListBox();
+
+        // Обработчик события нажатия на кнопку "Добавить"
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string task = textBoxTask.Text.Trim();
+            if (!string.IsNullOrEmpty(task))
+            {
+                listBoxTasks.Items.Add(task);
+                textBoxTask.Clear();
+            }
+        }
+
+        // Обработчик события нажатия на кнопку "Удалить"
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxTasks.SelectedIndex;
+            if (selectedIndex >= 0)
+            {
+                listBoxTasks.Items.RemoveAt(selectedIndex);
+            }
+        }
+
+        // Обработчик события нажатия на кнопку "Редактировать"
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBoxTasks.SelectedIndex;
+            if (selectedIndex >= 0)
+            {
+                string editedTask = ShowEditDialog(listBoxTasks.SelectedItem.ToString());
+                if (!string.IsNullOrEmpty(editedTask))
+                {
+                    listBoxTasks.Items[selectedIndex] = editedTask;
+                }
+            }
+        }
+
+        // Метод для отображения диалогового окна редактирования задачи
+        private string ShowEditDialog(string currentTask)
+        {
+            using (Form editForm = new Form())
+            {
+                editForm.Text = "Редактирование задачи";
+                editForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+                editForm.StartPosition = FormStartPosition.CenterScreen;
+
+                TextBox textBoxEdit = new TextBox();
+                textBoxEdit.Text = currentTask;
+                textBoxEdit.Dock = DockStyle.Fill;
+
+                Button buttonSave = new Button();
+                buttonSave.Text = "Сохранить";
+                buttonSave.DialogResult = DialogResult.OK;
+
+                editForm.Controls.Add(textBoxEdit);
+                editForm.Controls.Add(buttonSave);
+
+                if (editForm.ShowDialog() == DialogResult.OK)
+                {
+                    return textBoxEdit.Text.Trim();
+                }
+                else
+                {
+                    return currentTask;
+                }
+            }
+        }
+    }
+}
